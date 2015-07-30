f1289643c149966060d15b9b47477e913fb14cb6
Raúl Jiménez <jimenezjrs@gmail.com>
2015-07-30 11:46:38 -0500

    Added youtube videos and some basic styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,10 @@
 // jshint devel:true
-console.log('\'Allo \'Allo!');
+app.controller('MainCtrl', [function(){
+  this.videos = [
+    // {title: 'Título one', description: 'Some description one', url: 'sMKoNBRZM1M'},
+    {title: 'Título two', description: 'Some description two', url: 'https://www.youtube.com/watch?v=ugxCocs75zE'},
+    {title: 'Título three', description: 'Some description three', url: 'https://www.youtube.com/watch?v=OXiOm_nU7AE'},
+    {title: 'Título four', description: 'Some description fourth', url: 'https://www.youtube.com/watch?v=tbIKNeY_LX4'},
+    {title: 'Título five', description: 'Some description five', url: 'https://www.youtube.com/watch?v=hrCOOF_z6mc'},
+  ];
+}]);
