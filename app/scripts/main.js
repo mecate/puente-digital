a5dce5153399ff96296cf9f39c230b639b217d31
Raúl Jiménez <jimenezjrs@gmail.com>
2015-07-30 15:16:13 -0500

    added a little margin between the intro and videos, installed font awesome

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,9 @@
 // jshint devel:true
-app.controller('MainCtrl', ['youtubeEmbedUtils', function(youtubeEmbedUtils){
+app.controller('MainCtrl', [function(){
   this.videos = [
-    // {title: 'Título one', description: 'Some description one', url: 'sMKoNBRZM1M'},
-    {title: 'Título two', description: 'Some description two', url: 'https://www.youtube.com/watch?v=ugxCocs75zE', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
-    {title: 'Título three', description: 'Some description three', url: 'https://www.youtube.com/watch?v=OXiOm_nU7AE', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
-    {title: 'Título four', description: 'Some description fourth', url: 'https://www.youtube.com/watch?v=tbIKNeY_LX4', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
-    {title: 'Título five', description: 'Some description five', url: 'https://www.youtube.com/watch?v=hrCOOF_z6mc', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
+    {title: 'Título uno', description: 'Some description two', url: 'https://www.youtube.com/watch?v=ugxCocs75zE', duration: '2:42' },
+    {title: 'Título dos', description: 'Some description three', url: 'https://www.youtube.com/watch?v=OXiOm_nU7AE', duration: '2:27' },
+    {title: 'Título tres', description: 'Some description fourth', url: 'https://www.youtube.com/watch?v=tbIKNeY_LX4', duration: '3:21' },
+    {title: 'Título cuantro', description: 'Some description five', url: 'https://www.youtube.com/watch?v=hrCOOF_z6mc', duration: '15:01' },
   ];
-
 }]);
