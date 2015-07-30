a9c192ce4810fc171fcf3b65e8e9f438ceb534c7
Raúl Jiménez <jimenezjrs@gmail.com>
2015-07-30 13:50:00 -0500

    added some basic styling and change the accent colors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,11 @@
 // jshint devel:true
-app.controller('MainCtrl', [function(){
+app.controller('MainCtrl', ['youtubeEmbedUtils', function(youtubeEmbedUtils){
   this.videos = [
     // {title: 'Título one', description: 'Some description one', url: 'sMKoNBRZM1M'},
-    {title: 'Título two', description: 'Some description two', url: 'https://www.youtube.com/watch?v=ugxCocs75zE'},
-    {title: 'Título three', description: 'Some description three', url: 'https://www.youtube.com/watch?v=OXiOm_nU7AE'},
-    {title: 'Título four', description: 'Some description fourth', url: 'https://www.youtube.com/watch?v=tbIKNeY_LX4'},
-    {title: 'Título five', description: 'Some description five', url: 'https://www.youtube.com/watch?v=hrCOOF_z6mc'},
+    {title: 'Título two', description: 'Some description two', url: 'https://www.youtube.com/watch?v=ugxCocs75zE', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
+    {title: 'Título three', description: 'Some description three', url: 'https://www.youtube.com/watch?v=OXiOm_nU7AE', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
+    {title: 'Título four', description: 'Some description fourth', url: 'https://www.youtube.com/watch?v=tbIKNeY_LX4', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
+    {title: 'Título five', description: 'Some description five', url: 'https://www.youtube.com/watch?v=hrCOOF_z6mc', duration: youtubeEmbedUtils.getTimeFromURL(this.url) },
   ];
+
 }]);
