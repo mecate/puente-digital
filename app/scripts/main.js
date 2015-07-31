4ff06718dfc0066b86dd5f9a801b703b1dc9aa13
Raúl Jiménez <jimenezjrs@gmail.com>
2015-07-30 20:55:11 -0500

    Added video to the login

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,8 @@
 // jshint devel:true
 app.controller('MainCtrl', [function(){
   this.videos = [
-    {title: 'Título uno', category: '#gobierno', description: 'Some description two', url: 'https://www.youtube.com/watch?v=ugxCocs75zE', duration: '2:42' },
-    {title: 'Título dos', category: '#gobierno', description: 'Some description three', url: 'https://www.youtube.com/watch?v=OXiOm_nU7AE', duration: '2:27' },
-    {title: 'Título tres', category: '#gobierno', description: 'Some description fourth', url: 'https://www.youtube.com/watch?v=tbIKNeY_LX4', duration: '3:21' },
-    {title: 'Título cuantro', category: '#herramientas', description: 'Some description five', url: 'https://www.youtube.com/watch?v=hrCOOF_z6mc', duration: '15:01' },
+    {title: 'Título uno', category: '#gobierno', description: 'Some description two', url: 'https://www.youtube.com/watch?v=ugxCocs75zE?rel=0', duration: '2:42' },
+    {title: 'Título dos', category: '#gobierno', description: 'Some description three', url: 'https://www.youtube.com/watch?v=OXiOm_nU7AE?rel=0', duration: '2:27' },
+    {title: 'Título tres', category: '#gobierno', description: 'Some description fourth', url: 'https://www.youtube.com/watch?v=tbIKNeY_LX4?rel=0', duration: '3:21' },
   ];
 }]);
